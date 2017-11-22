2b8b3d62b419d631841b0ec0ed398dcf7e4d1a77
Ben <bes1002t@web.de>
2017-11-22 16:39:49 +0000

    Add new tests to access sensors via i2c.

--- PiDrone/src/main.rs
+++ PiDrone/src/main.rs
@@ -1,53 +1,20 @@
-extern crate i2cdev;
-extern crate docopt;
 
-use std::thread;
-use std::time::Duration;
-use std::env::args;
-use docopt::Docopt;
-use i2cdev::sensors::{Thermometer, Barometer, Accelerometer};
-use i2cdev::sensors::mpl115a2_barometer::*;
-use i2cdev::sensors::adxl345_accelerometer::*;
-#[cfg(any(target_os = "linux", target_os = "android"))]
-use i2cdev::linux::*;
-
-const USAGE: &'static str = "
-Reading sensor data from a variety of sensors
-Usage:
-  sensors <device>
-  sensors (-h | --help)
-  sensors --version
-Options:
-  -h --help    Show this help text.
-  --version    Show version.
-";
-
-
-#[cfg(not(any(target_os = "linux", target_os = "android")))]
-fn main() {}
-
-#[cfg(any(target_os = "linux", target_os = "android"))]
 fn main() {
-    let args = Docopt::new(USAGE)
-                   .and_then(|d| d.argv(args().into_iter()).parse())
-                   .unwrap_or_else(|e| e.exit());
-    let device = args.get_str("<device>");
-    let mpl115a2_i2cdev = LinuxI2CDevice::new(device, MPL115A2_I2C_ADDR).unwrap();
-    let adxl345_i2cdev = LinuxI2CDevice::new(device, 0x53).unwrap();
-
-    let mut mpl115a2 = MPL115A2BarometerThermometer::new(mpl115a2_i2cdev).unwrap();
-    let mut adxl345 = ADXL345Accelerometer::new(adxl345_i2cdev).unwrap();
-
-    println!("== ADXL345 ID: 0x{:X} ==", adxl345.device_id().unwrap());
-
-    loop {
-        let accel = adxl345.accelerometer_sample().unwrap();
-        println!("Temperature: {:?} C",
-                 mpl115a2.temperature_celsius().unwrap());
-        println!("Pressure:    {:?} kPa", mpl115a2.pressure_kpa().unwrap());
-        println!("Accel:       {:?}", accel);
-        println!("Accel Tot:   {:?}",
-                 (accel.x.powi(2) + accel.y.powi(2) + accel.z.powi(2)).sqrt());
-        thread::sleep(Duration::from_millis(1000));
-    }
+	let sensor = BMP180_Barometer::new();
+//    let hmc5883l_compass      = linuxi2cdevice::new(device, 0x3D);
+//    let mpu6050_accelerometer = linuxi2cdevice::new(device, 0x68);
+//    let mpu6050_gyroscope     = linuxi2cdevice::new(device, 0x68);
+//    let bmp180_barometer      = linuxi2cdevice::new(device, 0x77);
+//
+//    loop {
+//        bmp180_barometer.smbus_read_word_data(
+//        let accel = adxl345.accelerometer_sample().unwrap();
+//        println!("Temperature: {:?} C",
+//                 mpl115a2.temperature_celsius().unwrap());
+//        println!("Pressure:    {:?} kPa", mpl115a2.pressure_kpa().unwrap());
+//        println!("Accel:       {:?}", accel);
+//        println!("Accel Tot:   {:?}",
+//                 (accel.x.powi(2) + accel.y.powi(2) + accel.z.powi(2)).sqrt());
+//        thread::sleep(Duration::from_millis(1000));
+//    }
 }
---
+++ PiDrone/src/utils/pi_i2c_device.rs
@@ -0,0 +1,35 @@
+use docopt::Docopt;
+use i2cdev::linux::*;
+
+struct Pi_I2C_Device {
+	i2c_device: i2cdevice
+}
+
+impl Pi_I2C_Device {
+	// If Raspberry Pi revision = 1 this value should be "/dev/i2c-0"
+	device_uri = "/dev/i2c-1"
+
+	pub fn new(hw_addr) -> I2cDevice {
+		i2c_device = linuxi2cdevice::new(device_uri, hw_addr);
+	}
+
+	pub fn readU16(register) -> Result<(u16), MyError> {
+		value = try!(i2c_device.smbus_read_word_data(self, register))
+		Ok(value)
+	}
+
+	pub fn writeU16(register, value) -> Result<(), MyError> {
+		try!(i2c_device.smbus_write_word_data(self, register, value))
+		Ok(())
+	}
+
+	pub fn readU8(register) -> Result<(u8), MyError> {
+		value = try!(i2c_device.smbus_read_byte_data(self, register))
+		Ok(value)
+	}
+
+	pub fn writeU8(register, value) -> Result<(), MyError> {
+		try!(i2c_device.smbus_write_byte_data(self, register, value))
+		Ok(())
+	}
+}
